2ca8b765c9598c95d0a33bb24de674965aa7783c
tony09uk <tony09uk@gmail.com>
2021-04-22 20:35:32 +0100

    Add project files.

---
+++ Presentation/Nop.Web/Plugins/Misc.MySmartCards/Assets/javascript/customer/handwriting.js
@@ -0,0 +1,38 @@
+﻿let fontPreview;
+let fontOptionSelect;
+let fontSizeSelect;
+let selectedFontSizeId;
+let selectedFontOptionsId;
+
+window.addEventListener('load', (event) => {
+    setVariables();
+    setEventListeners();
+});
+
+function setVariables() {
+    fontPreview = document.getElementById("fontPreview");
+    fontOptionSelect = document.getElementById("fontOptions");
+    fontSizeSelect = document.getElementById("fontSizes");
+    selectedFontSizeId = document.getElementById("selectedFontSizeId");
+    selectedFontOptionsId = document.getElementById("selectedFontOptionsId");
+}
+
+function setEventListeners() {
+    fontOptionSelect.addEventListener("change", setFont);
+    fontSizeSelect.addEventListener("change", setSize);
+}
+
+function setFont() {
+    var selected = fontOptionSelect.options[fontOptionSelect.selectedIndex];
+    let family = selected.value.substring(0, selected.value.indexOf('-') - 1);
+    let id = selected.value.substring(selected.value.indexOf('-') + 2);
+
+    fontPreview.style.fontFamily = family;
+    selectedFontOptionsId.value = id;
+}
+
+function setSize() {
+    var selected = fontSizeSelect.options[fontSizeSelect.selectedIndex];
+    fontPreview.style.fontSize = selected.text;
+    selectedFontSizeId.value = selected.value;
+}
